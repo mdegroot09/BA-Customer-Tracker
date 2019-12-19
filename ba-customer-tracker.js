bd0e3e10cf1b55301dd994e7e358a037830efbcb
Mike De Groot <mdegroot09@gmail.com>
2019-12-19 14:39:36 -0700

    added archive Closed conversion

--- ba-customer-tracker.js
+++ ba-customer-tracker.js
@@ -69,12 +69,28 @@ function onEdit(e){
   }
   
   // Check for status change in Archive tab
-  else if (sheetName === 'Archive' && columnEdited === 12){
+  else if (sheetName === 'Archive' && (columnEdited === 7 || columnEdited === 12)){
     var cell = range.getA1Notation()
     var val = ss.getRange(cell).getValue()
     
-    // Moving from Archive
-    if ((val === 'Open') && columnEdited === 12){
+    // Moving from Archive by changing stage with Open status
+    if (columnEdited === 7 && ss.getRange("L"+rowEdited).getValue() === 'Open'){
+      if (val === 'Searching' || val === 'Touring' || val === 'Offering' || val === 'UC'){
+        ss.getRange(cell).setBackground(null)
+        convertToOpp(rowEdited)
+      }
+      else if (val === 'Warm Lead' || val === 'New Lead'){
+        ss.getRange(cell).setBackground(null)
+        convertToWarm(rowEdited)
+      }
+      else if (val === 'Cold Lead'){
+        ss.getRange(cell).setBackground(null)
+        convertToCold(rowEdited)
+      }
+    }
+    
+    // Moving from Archive by changing status
+    else if (val === 'Open' && columnEdited === 12){
       
       var stageCell = "G"+rowEdited+""
       var stage = ss.getRange(stageCell).getValue()
